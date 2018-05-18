7d3a31722aae25c3814d63bd087d6242d0562060
ac-agogoi <ac-agogoi@192.168.100.82>
2018-05-18 14:25:05 -0300

    mvn clean install creates the jar assembly only

--- archetype-porua/src/main/java/com/porua/app/MyApp.java
+++ archetype-porua/src/main/java/com/porua/app/MyApp.java
@@ -1,20 +1,13 @@
 package com.porua.app;
 
 import java.io.File;
-import java.net.URL;
 
 import com.porua.container.PoruaContainer;
 
 public class MyApp {
 
 	public static void main(String[] a) throws Exception {
-		porua();
+		PoruaContainer.scanSingleApp(new File("./target/archetype-porua-1.0.0.jar"));
 	}
 
-	public static void porua() throws Exception {
-		String file1 = "file:./src/main/app/my-app-1.xml";
-		String[] apps = new String[] { file1 };
-		URL jarUrl = new File("./target/archetype-porua-1.0.0.jar").toURI().toURL();
-		PoruaContainer.loadSingleApp(jarUrl, apps);
-	}
 }
