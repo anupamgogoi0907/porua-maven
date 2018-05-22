851f700711a3af5c59d0e87dce09ad2c920d6f57
ac-agogoi <ac-agogoi@192.168.100.82>
2018-05-22 15:36:41 -0300

    archetype modified

--- archetype-connector/target/generated-sources/archetype/src/main/resources/archetype-resources/src/main/java/com/custom/connector/ConnectorOne.java
+++ archetype-connector/target/generated-sources/archetype/src/main/resources/archetype-resources/src/main/java/com/custom/connector/ConnectorOne.java
@@ -1,9 +1,12 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
 package com.custom.connector;
 
 /*
-import com.porua.core.processor.MessageProcessor;
-import com.porua.core.tag.Connector;
-import com.porua.core.tag.ConnectorConfig;
+import ${groupId}.core.processor.MessageProcessor;
+import ${groupId}.core.tag.Connector;
+import ${groupId}.core.tag.ConnectorConfig;
 
 @Connector(tagName = "con-one", tagNamespace = "http://www.connector.com/custom", tagSchemaLocation = "http://www.connector.com/custom/custom.xsd", imageName = "")
 public class ConnectorOne extends MessageProcessor {
---
+++ archetype-connector/target/generated-sources/archetype/src/main/resources/archetype-resources/src/main/java/com/custom/connector/ConnectorOneConfig.java
@@ -0,0 +1,36 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package com.custom.connector;
+
+public class ConnectorOneConfig {
+
+	private String name;
+	private String host;
+	private int port;
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
