75e5b2c5547dbcbe7445bae5a2eeff523b63af06
anupamgogoi@gmail.com <anupamgogoi@gmail.com>
2018-06-18 14:28:33 -0300

    Eclipse aether features added

--- codegen-maven-plugin/src/main/java/com/porua/mojo/GenPaletteMojo.java
+++ codegen-maven-plugin/src/main/java/com/porua/mojo/GenPaletteMojo.java
@@ -9,6 +9,9 @@ import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.repository.RemoteRepository;
 
 import com.porua.codegen.GenerateCode;
 import com.porua.utility.MojoUtility;
@@ -24,11 +27,20 @@ public class GenPaletteMojo extends AbstractMojo {
 	@Component
 	private MavenProject mavenProject;
 
+	@Component
+	private RepositorySystem repoSystem;
+
+	@Parameter(defaultValue = "${repositorySystemSession}")
+	private RepositorySystemSession session;
+
+	@Parameter(defaultValue = "${project.remoteProjectRepositories}")
+	private List<RemoteRepository> remoteRepositories;
+
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		try {
 			if (connectors != null) {
-				ClassLoader loader = MojoUtility.getClassLoader(mavenProject);
+				ClassLoader loader = MojoUtility.getClassLoader(mavenProject, repoSystem, session, remoteRepositories);
 				GenerateCode.generatePaletteAssets(pkg, connectors, loader);
 			}
 
--- codegen-maven-plugin/src/main/java/com/porua/utility/MojoUtility.java
+++ codegen-maven-plugin/src/main/java/com/porua/utility/MojoUtility.java
@@ -1,26 +1,65 @@
 package com.porua.utility;
 
-import java.io.File;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.maven.project.MavenProject;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.repository.RemoteRepository;
+import org.eclipse.aether.resolution.ArtifactRequest;
+import org.eclipse.aether.resolution.ArtifactResult;
 
+@SuppressWarnings("unchecked")
 public class MojoUtility {
 
 	private static ClassLoader loader = null;
 
-	public static ClassLoader getClassLoader(MavenProject mavenProject) throws Exception {
+	public static ClassLoader getClassLoader(MavenProject mavenProject, RepositorySystem repoSystem,
+			RepositorySystemSession session, List<RemoteRepository> remoteRepositories) throws Exception {
+
 		if (MojoUtility.loader == null) {
-			List<?> runtimeClasspathElements = mavenProject.getRuntimeClasspathElements();
-			URL[] runtimeUrls = new URL[runtimeClasspathElements.size()];
-			for (int i = 0; i < runtimeClasspathElements.size(); i++) {
-				String element = (String) runtimeClasspathElements.get(i);
-				runtimeUrls[i] = new File(element).toURI().toURL();
-			}
-			MojoUtility.loader = new URLClassLoader(runtimeUrls, Thread.currentThread().getContextClassLoader());
+			List<URL> listDependency = getProjectDependencies(mavenProject, repoSystem, session, remoteRepositories);
+			MojoUtility.loader = new URLClassLoader(listDependency.toArray(new URL[listDependency.size()]), Thread.currentThread().getContextClassLoader());
 		}
 		return MojoUtility.loader;
+	}
+
+	/**
+	 * Get dependent artifact jars.
+	 * {@link https://vzurczak.wordpress.com/2016/01/08/finding-dependencies-artifacts-in-your-maven-plug-in/}
+	 * 
+	 * @param mavenProject
+	 * @param repoSystem
+	 * @param session
+	 * @param remoteRepositories
+	 * @return
+	 * @throws Exception
+	 */
+	public static List<URL> getProjectDependencies(MavenProject mavenProject, RepositorySystem repoSystem,
+			RepositorySystemSession session, List<RemoteRepository> remoteRepositories) throws Exception {
+		List<URL> list = new ArrayList<>();
+
+		// Get the dependent artifacts.
+		Set<org.apache.maven.artifact.Artifact> set = mavenProject.getDependencyArtifacts();
+		set.add(mavenProject.getArtifact());
+
+		// Get the URL of each artifact.
+		Iterator<org.apache.maven.artifact.Artifact> itr = mavenProject.getDependencyArtifacts().iterator();
+		while (itr.hasNext()) {
+			org.apache.maven.artifact.Artifact unresolvedArtifact = itr.next();
+
+			org.eclipse.aether.artifact.Artifact aetherArtifact = new DefaultArtifact(unresolvedArtifact.getGroupId(), unresolvedArtifact.getArtifactId(), unresolvedArtifact.getClassifier(), unresolvedArtifact.getType(), unresolvedArtifact.getVersion());
+			ArtifactRequest request = new ArtifactRequest().setArtifact(aetherArtifact).setRepositories(remoteRepositories);
+			ArtifactResult result = repoSystem.resolveArtifact(session, request);
+			list.add(result.getArtifact().getFile().toURI().toURL());
+
+		}
+		return list;
 	}
 }
